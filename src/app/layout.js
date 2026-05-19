39223c5b544a38559976f9c0f5cc76b5ea356291
VictorAken <victoraken@tronweb.co>
2026-05-19 14:49:48 +0100

    Added Google Tag Manager

--- src/app/layout.js
+++ src/app/layout.js
@@ -2,6 +2,7 @@ import { DM_Sans, Manrope, Plus_Jakarta_Sans, Bebas_Neue, Source_Serif_4 } from 
 import "./globals.css";
 import Navbar from "@/components/layout/Navbar";
 import Footer from "@/components/layout/Footer";
+import { GoogleTagManager } from "@next/third-parties/google";
 
 // Fonts
 const dmSans = DM_Sans({
@@ -26,8 +27,7 @@ const bebasNeue = Bebas_Neue({
   subsets: ['latin'],
   weight: '400',
   variable: '--font-bebas',
-})
-
+});
 
 const sourceSerif = Source_Serif_4({
   subsets: ["latin"],
@@ -35,8 +35,6 @@ const sourceSerif = Source_Serif_4({
   style: ["normal", "italic"],
   variable: "--font-source-serif",
 });
-
-
 
 export const metadata = {
   title: "ClientForce",
@@ -47,8 +45,9 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body
-        className={` ${dmSans.variable} ${manrope.variable} ${pjs.variable} ${bebasNeue.variable} ${sourceSerif.variable} antialiased`}
+        className={`${dmSans.variable} ${manrope.variable} ${pjs.variable} ${bebasNeue.variable} ${sourceSerif.variable} antialiased`}
       >
+        <GoogleTagManager gtmId={process.env.NEXT_PUBLIC_GTM_ID} />
         <Navbar />
         {children}
         <Footer />
